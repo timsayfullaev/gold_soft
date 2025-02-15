669756d3ac05fa6f5ae0cf749b152ee8e70e1b84
Timur <tim.sayfullaev@gmail.com>
2025-02-15 16:59:29 +0500

    partners

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const swiperPartners = new Swiper('.swiper-partners', {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        992: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+        1200: {
+            slidesPerView: 6,
+        },
+    },
+});
